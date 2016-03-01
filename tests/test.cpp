6b70e6e3379b73437824f61723b222ec17b1b7a4
Chris Sawczuk <gummybassist@gmail.com>
2016-03-01 22:14:52 +0000

    Initial failing test for serving

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,6 +2,37 @@
 
 #include <gtest/gtest.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+
+static int create_socket()
+{
+	struct addrinfo hints, *res;
+	int sockfd;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	getaddrinfo("localhost", "8080", &hints, &res);
+
+	sockfd = socket(res->ai_family, res->ai_sockettype, res->ai_protocol);
+	ASSERT_NE(connect(sockfd, res->ai_addr, res->ai_addrlen), -1);
+	return sockfd;
+}
+
 TEST(WTWSTest, CanInstantiateAWebserver) {
 	auto w = wtws::Webserver();
+}
+
+TEST(WTWSTest, WebserverWillAcceptASocketConnection) {
+	auto w = wtws::Webserver();
+	w.startServing();
+
+	int connectedSocket = create_socket();
+
+	close(connectedSocket);
+
+	w.stopServing();
+
 }
